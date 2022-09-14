ad8b0a82407a716a9c3659bcbebd581a1c8f4cfb
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-14 12:36:56 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance } from "./components/utils/near";
+import Cover from "./components/utils/Cover";
+import Wallet from "./components/Wallet";
+import coverImg from "./cover.png";
+import "./App.css";
+import Functions from "./components/Functions"
+import Admin from "./components/admin"
 
-function App() {
+const App = function AppWrapper() {
+  const account = window.walletConnection.account();
+  const [balance, setBalance] = useState("0");
+
+
+  const getBalance = useCallback(async () => {
+    if (account.accountId) {
+      setBalance(await accountBalance());
+    }
+  });
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {account.accountId ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={account.accountId}
+                amount={balance}
+                symbol="NEAR"
+                destroy={destroy}
+              />
+            </Nav.Item>
+          </Nav>
+          <dev> <h1 align="middle">Verified Accounts</h1></dev>
+          <main>
+          {account.accountId === "kareemayman.testnet" ? (
+            <Admin/>
+          ): 
+            (
+              <Functions/>
+            )
+          }
+          </main>
+        </Container> 
+      ) : (
+        <Cover name="Verified Accounts" login={login} coverImg={coverImg}/>
+      )}
+    </>
   );
 }
 
