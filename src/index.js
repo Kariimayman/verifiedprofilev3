30976705a24dd8988a0caf4248536e01ab302750
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-14 13:53:00 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { initializeContract } from "./components/utils/near";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Buffer } from 'buffer';
 
+// @ts-ignore
+window.Buffer = Buffer;
 window.nearInitPromise = initializeContract()
   .then(() => {
     ReactDOM.render(
